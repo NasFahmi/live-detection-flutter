58ca98b4912961ae220834e0abeda6ed44a536c8
75nasrulfahmi <75nasrulfahmi@gmail.com>
2024-09-02 11:51:43 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:myapp/screen/home.dart';
+
+void main() {
+  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Home(),
+    );
+  }
+}
---
+++ lib/screen/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/screen/camera_screen.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        child: Center(
+          child: TextButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => CameraScreen()));
+            },
+            child: Text('Open Camera'),
+          ),
+        ),
+      )),
+    );
+  }
+}
